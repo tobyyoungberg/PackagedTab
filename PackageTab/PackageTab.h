eec98b613e0166a06eef62b91d0a9000208875f7
Toby Youngberg <tyoungberg@sofi.org>
2019-08-19 13:23:02 -0600

    Initial Commit

---
+++ PackageTab/PackageTab.h
@@ -0,0 +1,19 @@
+//
+//  PackageTab.h
+//  PackageTab
+//
+//  Created by Toby Youngberg on 8/19/19.
+//  Copyright © 2019 Toby Youngberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PackageTab.
+FOUNDATION_EXPORT double PackageTabVersionNumber;
+
+//! Project version string for PackageTab.
+FOUNDATION_EXPORT const unsigned char PackageTabVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PackageTab/PublicHeader.h>
+
+
